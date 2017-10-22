1febd2725971227d29f1c370ed43e4d6927ad22e
Josh Beard <josh@jlbeard.com>
2017-10-22 16:05:46 -0400

    Moved project to ng.

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import * as Components from '../components';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Components.HomeComponent,
+    Components.PhotosComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: Components.HomeComponent
+      },
+      {
+        path: 'photos',
+        component: Components.PhotosComponent
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent,
+    Components.HomeComponent,
+    Components.PhotosComponent
+  ]
+})
+export class AppModule { }
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { HomeComponent } from './home.component';
+export { PhotosComponent } from './photos.component';
